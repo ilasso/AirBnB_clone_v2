c5da72560dbf9db224be8274043664ff7936ea14
ilasso <1204@holbertonschool.com>
2020-04-02 06:53:19 +0000

    init

--- models/base_model.py
+++ models/base_model.py
@@ -63,18 +63,14 @@ class BaseModel:
             updated_at: updated date
         """
         if kwargs:
+            if self.id is None:
+                self.id = str(uuid.uuid4())
+
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__" and hasattr(self, key):
+                if key != "__class__":
                     setattr(self, key, value)
-
-            if self.id is None:
-                setattr(self, 'id', str(uuid.uuid4()))
-
-            stamp = datetime.now()
-            if self.created_at is None:
-                self.created_at = stamp
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
@@ -101,6 +97,7 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        self.created_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
