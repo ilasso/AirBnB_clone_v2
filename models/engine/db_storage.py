115a15235e7c9b8354ef1318f5d4438c963f9aac
KevCasHSC <castrok.c11@hotmail.com>
2020-04-02 02:11:58 +0000

    last upload

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,7 +11,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-import os
+from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
