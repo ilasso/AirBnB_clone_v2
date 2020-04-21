1cf506fdeb84c3f7124b3bfa6e486a2fec7ca168
ilasso <1204@holbertonschool.com>
2020-04-21 03:48:00 +0000

    task3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+    Basic WebApplication
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Basic WebApplication
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def only_hbnb():
+    """
+    Basic WebApplication
+    """
+    return 'HBNB!'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def Cis(text):
+    """
+    Basic WebApplication
+    """
+    return 'C'+' '+text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+def pythonis(text):
+    """
+    Basic WebApplication
+    """
+    return 'Python'+' '+text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def pythonis2(text):
+    """
+    Basic WebApplication
+    """
+    return 'Python'+' '+text.replace('_', ' ')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
