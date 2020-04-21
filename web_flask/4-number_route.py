a1860f34eced7e7d4cc5234c2eeeec59e27b38d5
ilasso <1204@holbertonschool.com>
2020-04-21 04:11:00 +0000

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+    Basic WebApplication
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Basic WebApplication
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def only_hbnb():
+    """
+    Basic WebApplication
+    """
+    return 'HBNB!'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def Cis(text):
+    """
+    Basic WebApplication
+    """
+    return 'C'+' '+text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+def pythonis(text):
+    """
+    Basic WebApplication
+    """
+    return 'Python'+' '+text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def pythonis2(text):
+    """
+    Basic WebApplication
+    """
+    return 'Python'+' '+text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def nisanumber(n):
+    """
+    Basic WebApplication
+    """
+    return 'n is a number'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
